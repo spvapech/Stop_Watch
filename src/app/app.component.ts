6d9751450e918519cf2ba56f6b99e65385c05eb3
Vaios <pechlevanidis.vaios@gmail.com>
2023-12-06 16:59:59 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,44 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'stopWatch';
+
+  ms: any = '0' + 0;
+  sec: any = '0' + 0;
+  min: any = '0' + 0;
+
+  startTimer: any;
+  running = false;
+
+  start(): void {
+    if (!this.running) {
+      this.running = true; // Use boolean value directly
+      this.startTimer = setInterval(() => {
+        this.ms++;
+        this.ms = this.ms < 10 ? '0' + this.ms : this.ms;
+
+        if (this.ms === 100) {
+          this.sec++;
+          this.sec = this.sec < 10 ? '0' + this.sec : this.sec;
+          this.ms = '0' + 0;
+        }
+
+        if (this.sec === 60) {
+          this.min++;
+          this.min = this.min < 10 ? '0' + this.min : this.min;
+          this.sec = '0' + 0;
+        }
+      }, 10); // Update the interval to 10 milliseconds
+    }
+  }
+
+  stop(): void {
+    clearInterval(this.startTimer);
+    this.running = false;
+  }
+
+  reset(): void {
+    clearInterval(this.startTimer);
+    this.running = false;
+    this.min = this.sec = this.ms = '0' + 0;
+  }
 }
